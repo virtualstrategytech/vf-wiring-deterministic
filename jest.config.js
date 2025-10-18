5bf8784acf79cb7f182c7125db81d527b9a55db0
peaisgreen <47340974+peaisgreen@users.noreply.github.com>
2025-10-17 21:18:53 -0400

    chore(tests): remove duplicate jest.config.js (use jest.config.cjs)

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-﻿module.exports = {
-  testEnvironment: 'node',
-  globalSetup: '<rootDir>/tests/globalSetup.js',
-  globalTeardown: '<rootDir>/tests/globalTeardown.js',
-  testMatch: ['**/tests/**/*.test.js'],
-  detectOpenHandles: true,
-  testTimeout: 30000
-};
