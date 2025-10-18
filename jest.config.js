f0dd8f5cbb6391dc6a2567e0d955734014ab079c
peaisgreen <47340974+peaisgreen@users.noreply.github.com>
2025-10-17 21:14:03 -0400

    chore(tests): add jest.config.js and workspace just path

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+﻿module.exports = {
+  testEnvironment: 'node',
+  globalSetup: '<rootDir>/tests/globalSetup.js',
+  globalTeardown: '<rootDir>/tests/globalTeardown.js',
+  testMatch: ['**/tests/**/*.test.js'],
+  detectOpenHandles: true,
+  testTimeout: 30000
+};
