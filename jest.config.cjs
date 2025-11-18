53b7e7f39e5cf8475c54e6481f6e95493c38e719
peaisgreen <47340974+peaisgreen@users.noreply.github.com>
2025-11-17 20:44:37 -0500

    ci: merge child-server workflow (strict+loose+manual) and keep local test config

--- jest.config.cjs
+++ jest.config.cjs
@@ -5,8 +5,5 @@ module.exports = {
   setupFilesAfterEnv: ["<rootDir>/tests/jest.setup.js"],
   globalSetup: "<rootDir>/tests/globalSetup.js",
   globalTeardown: "<rootDir>/tests/globalTeardown.js",
-  testTimeout: Number(process.env.JEST_TIMEOUT || 10000),
-  // ignore legacy package to avoid haste-map name collision
-  modulePathIgnorePatterns: ["<rootDir>/webhook.legacy/"],
-  collectCoverage: false,
+  verbose: true,
 };
