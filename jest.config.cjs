da3da580e3d91fcc4bc9b2dbb4372b23205bb329
peaisgreen <47340974+peaisgreen@users.noreply.github.com>
2025-11-17 19:37:05 -0500

    ci(test): add jest.setup and child-process CI workflow; ignore legacy for Jest

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,7 +1,12 @@
-﻿module.exports = {
+﻿// jest.config.cjs
+module.exports = {
   testEnvironment: "node",
-  testTimeout: 15000,
-  globalSetup: "./tests/globalSetup.js",
-  globalTeardown: "./tests/globalTeardown.js",
-  reporters: ["default"],
+  roots: ["<rootDir>/tests"],
+  setupFilesAfterEnv: ["<rootDir>/tests/jest.setup.js"],
+  globalSetup: "<rootDir>/tests/globalSetup.js",
+  globalTeardown: "<rootDir>/tests/globalTeardown.js",
+  testTimeout: Number(process.env.JEST_TIMEOUT || 10000),
+  // ignore legacy package to avoid haste-map name collision
+  modulePathIgnorePatterns: ["<rootDir>/webhook.legacy/"],
+  collectCoverage: false,
 };
